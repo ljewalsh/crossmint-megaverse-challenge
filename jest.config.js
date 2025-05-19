b75f9ad437a35abcf486e411ca72c9ed0b5294be
Lucy-Jane <ljewalsh@gmail.com>
2025-05-19 12:46:21 +0200

    Set verbose to true in jest config

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ const tsJestTransformCfg = createDefaultPreset().transform;
 
 /** @type {import("jest").Config} **/
 module.exports = {
+  verbose: true,
   setupFiles: ["dotenv/config"],
   testEnvironment: "node",
   transform: {
